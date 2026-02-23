70afa3a5717cab9b7fc3f7a50b04109355f6bbb7
Nathasha <142737136+Nathasha2001@users.noreply.github.com>
2026-02-23 21:03:20 +0530

    COde Optimization

--- src/main/java/com/example/BookApplication/Entity/Book.java
+++ src/main/java/com/example/BookApplication/Entity/Book.java
@@ -4,9 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+
 
 @Entity
+@Data
 public class Book {
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,39 +20,5 @@ public class Book {
     private String author;
     private String genre;
 
-    // ✅ REQUIRED: No-args constructor
-    public Book() {}
 
-    // ✅ Getters & Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
 }
